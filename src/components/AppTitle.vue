97927e0bce399412b923ba14e5bcf1cc6a9e44c5
Mustaaaaa <moustafaibrahim1512@gmail.com>
2024-04-12 14:41:18 +0200

    s

--- src/components/AppTitle.vue
+++ src/components/AppTitle.vue
@@ -3,7 +3,8 @@
     export default{
         data(){
             return{
-                title: 'La mia prima app con Vite!'
+                title: 'امير درويش',
+                subtitle: 'امير درويش'
             }
         }
     }
@@ -12,13 +13,15 @@
 
 <template>
     <h1 class="title">{{ title }}</h1>
+    <h2>{{ subtitle }}</h2>
 </template>
 
 <style>  
 .title {
     font-size: 50px;
     color: red;
-    display: flex;
+
 }
+
 
 </style>
